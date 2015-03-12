d8d351b218c6ad6be92e77abe00fd7e2986bbe76
lalotone <lalotone@gmail.com>
2015-03-12 23:51:46 +0100

    Agregado mapas

--- app/src/main/java/com/elpoeta/menulateralslide/Mapa.java
+++ app/src/main/java/com/elpoeta/menulateralslide/Mapa.java
@@ -4,13 +4,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.elpoeta.menulateralslide.database.orm.Seas01;
+import com.elpoeta.menulateralslide.staticdata.ListaElementos;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class Mapa extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,10 +38,13 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
 
         // You can customize the marker image using images bundled with
         // your app, or dynamically generated bitmaps.
-        map.addMarker(new MarkerOptions()
+        //***********************
+        //Aqui estaba creado el primer punto
+        //***********************
+        /*map.addMarker(new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
                 .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .position(new LatLng(41.889, -87.622)));
+                .position(new LatLng(41.889, -87.622)));*/
     }
 
 
@@ -46,27 +54,28 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
+            //ListaElementos listaEles = new ListaElementos();
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            ArrayList<Seas01> proof = ListaElementos.getDatosSeas();
+            for (int i = 0; i < proof.size(); i++) {
+                String lon = proof.get(i).getLongitud();
+                String lat = proof.get(i).getLatitud();
+                double lonD = Double.parseDouble(lon);
+                double latD = Double.parseDouble(lat);
+                LatLng point = new LatLng(lonD, latD);
+                MarkerOptions marcador = new MarkerOptions();
+                marcador.position(point);
+                mMap.addMarker(marcador);
+            }
+
+
+            //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            //       .getMap(); EL ORIGINAL
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
@@ -74,13 +83,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //smMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
---
+++ app/src/main/java/com/elpoeta/menulateralslide/PantallaDescripcion.java
@@ -0,0 +1,13 @@
+package com.elpoeta.menulateralslide;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by lalotone on 12/03/15.
+ */
+public class PantallaDescripcion {
+    private ImageView img;
+    private TextView txtDescripcion;
+
+}
