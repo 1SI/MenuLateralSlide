e96994b6707ac3fa5803fd254c43e5bbbecc7d22
Alejandro S <a18939@svalero.com>
2015-03-12 19:37:42 +0100

    Avanzando

--- app/src/main/java/com/elpoeta/menulateralslide/database/orm/seas01.java
+++ app/src/main/java/com/elpoeta/menulateralslide/database/orm/seas01.java
@@ -5,4 +5,70 @@ package com.elpoeta.menulateralslide.database.orm;
  */
 public class seas01 {
 
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String longitud;
+    private String latitud;
+    private String imagen;
+
+    public seas01() {
+    }
+
+    public seas01(int id, String nombre, String descripcion, String longitud, String latitud, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.longitud = longitud;
+        this.latitud = latitud;
+        this.imagen = imagen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
